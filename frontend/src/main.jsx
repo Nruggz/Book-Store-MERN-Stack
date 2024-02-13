edab57961a623edf026e37e2dd2b9a06da00a8a4
Nruggz <86481527+Nruggz@users.noreply.github.com>
2024-02-13 10:06:57 -0500

    Initial

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { SnackbarProvider } from 'notistack';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <SnackbarProvider>
-      <App />
-    </SnackbarProvider>
-  </BrowserRouter>
-);
+    <App />
+  </BrowserRouter>,
+)
